a6477703b6ca0c0f81063906bf08141d767cdba5
HANYU ZHANG <zhang.hanyu3@northeastern.edu>
2023-10-13 16:50:29 -0700

    updated some codes

--- src/App.tsx
+++ src/App.tsx
@@ -2,78 +2,77 @@
  * @jest-environment jsdom
  */
 
-import { useState, useEffect } from 'react';
-import './App.css';
-import SpreadSheet from './Components/SpreadSheet';
+import { useState, useEffect, useCallback } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import SpreadSheet from "./Components/SpreadSheet";
+import SpreadsheetManager from "./Components/SpreadsheetManager"; // 请替换为您的SpreadsheetManager.tsx的路径
 
 function App() {
-
-
-  const [documentName, setDocumentName] = useState(getDocumentNameFromWindow());
-  //const memoryUsage = process.memoryUsage();
-  useEffect(() => {
-    if (window.location.href) {
-      setDocumentName(getDocumentNameFromWindow());
-    }
-  }, [getDocumentNameFromWindow]);
-
-
-
-  // for the purposes of this demo and for the final project
-  // we will use the window location to get the document name
-  // this is not the best way to do this, but it is simple
-  // and it works for the purposes of this demo
-  function getDocumentNameFromWindow() {
+  const getDocumentNameFromWindow = useCallback(() => {
     const href = window.location.href;
 
-    // remove  the protocol 
-    const protoEnd = href.indexOf('//');
+    // remove  the protocol
+    const protoEnd = href.indexOf("//");
     // find the beginning of the path
-    const pathStart = href.indexOf('/', protoEnd + 2);
+    const pathStart = href.indexOf("/", protoEnd + 2);
 
     if (pathStart < 0) {
       // there is no path
-      return '';
+      return "";
     }
     // get the first part of the path
-    const docEnd = href.indexOf('/', pathStart + 1);
+    const docEnd = href.indexOf("/", pathStart + 1);
     if (docEnd < 0) {
       // there is no other slash
       return href.substring(pathStart + 1);
     }
     // there is a slash
     return href.substring(pathStart + 1, docEnd);
+  }, []);
 
-  }
+  const [documentName, setDocumentName] = useState(getDocumentNameFromWindow());
+
+  useEffect(() => {
+    if (window.location.href) {
+      setDocumentName(getDocumentNameFromWindow());
+    }
+  }, [getDocumentNameFromWindow]);
 
   //callback function to reset the current URL to have the document name
   function resetURL(documentName: string) {
     // get the current URL
     const currentURL = window.location.href;
     // remove anything after the last slash
-    const index = currentURL.lastIndexOf('/');
+    const index = currentURL.lastIndexOf("/");
     const newURL = currentURL.substring(0, index + 1) + documentName;
     // set the URL
-    window.history.pushState({}, '', newURL);
+    window.history.pushState({}, "", newURL);
     // now reload the page
     window.location.reload();
   }
 
-  if (documentName === '') {
-    setDocumentName('test');
-    resetURL('test');
+  if (documentName === "") {
+    setDocumentName("test");
+    resetURL("test");
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <SpreadSheet documentName={documentName} />
-      </header>
-
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/spreadsheet/:id">
+              <SpreadSheet documentName={documentName} />
+            </Route>
+            <Route path="/">
+              <SpreadsheetManager />
+            </Route>
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
-
